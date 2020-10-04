1dfa66593ff69a3d54ac3e4ace8ec430e07e3f3a
storabic <storabic@users.noreply.github.com>
2020-10-04 22:00:52 +0300

    New funcs

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import discord
-import random
 from discord.ext import commands
-token = 'YOURTOKEN'
-bot = commands.Bot(command_prefix = '!')
-@bot.command(pass_context=True)
+from config import settings
+import random
+
+bot = commands.Bot(command_prefix = settings['prefix'])
+
+@bot.command()
 async def roll(ctx, arg):
+    author = ctx.message.author
     random.seed()
-    ans = 'Введите натуральное число n, чтобы получить случайное число от 1 до n\nВведите два целых числа n m, чтобы получить случайное число от n до m'
+    help_str = 'Введите натуральное число n, чтобы получить случайное число от 1 до n\nВведите два целых числа n m, чтобы получить случайное число от n до m'
     try:
+        if(len(arg) > 100):
+            await ctx.send(help_str)
+            return
         x = list(map(int, arg.split(' ')))
         if (len(x) == 1):
             ans = random.randint(1, x[0])
         else:
             ans = random.randint(x[0], x[1])
+        await ctx.send(f'{author.mention} рольнул {ans}')
     except:
-        pass
-    await ctx.send(ans)
-bot.run(token)
+        await ctx.send(help_str)
+bot.run(settings['token'])
---
+++ settings.py
@@ -0,0 +1,8 @@
+from secret import info
+
+settings = {
+    'token': info['token'],
+    'bot': 'RollerBot',
+    'id': info['id'],
+    'prefix': '!'
+}
